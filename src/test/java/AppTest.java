9afb4852b079915bea96f5752b252dafa80ae83d
snowlight-aemt <it.s.aemt@gmail.com>
2023-05-07 21:23:12 +0900

    Init - 2

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -6,7 +7,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class AppTest {
 
     @Test
+    @Tag("slow")
     void add() {
+        App app = new App();
+        Assertions.assertEquals(app.add(1, 2), 2);
+
+    }
+    @Test
+    @Tag("fast")
+    void add_f() {
         App app = new App();
         Assertions.assertEquals(app.add(1, 2), 3);
 
